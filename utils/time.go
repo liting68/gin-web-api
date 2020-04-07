ac1e76c4062b028e924a29feb18bf4f0c788874a
liting68 <liting68@163.com>
2020-04-07 16:26:31 +0800

    优化时间解析

--- utils/time.go
+++ utils/time.go
@@ -13,6 +13,12 @@ const (
 	LayoutDateMin            = "2006-01-02 15:04"
 	LayoutDate               = "2006-01-02"
 	LayoutShortdate          = "2006-1-2"
+	SlashDatetime            = "2006/01/02 15:04:05"
+	SlashShortdateTime       = "2006/1/2 15:04:05"
+	SlashShortdateShortTime  = "2006/1/2 15:4:5"
+	SlashDateMin             = "2006/01/02 15:04"
+	SlashDate                = "2006/01/02"
+	SlashShortdate           = "2006/1/2"
 )
 
 //Str2Time 格式化时间
@@ -26,6 +32,12 @@ func Str2Time(timestr string) (t time.Time, err error) {
 		t, err = time.ParseInLocation(LayoutDate, string(sv), loc)
 		if err != nil {
 			t, err = time.ParseInLocation(LayoutShortdate, string(sv), loc)
+			if err != nil {
+				t, err = time.ParseInLocation(SlashDate, string(sv), loc)
+				if err != nil {
+					t, err = time.ParseInLocation(SlashShortdate, string(sv), loc)
+				}
+			}
 		}
 	} else {
 		t, err = time.ParseInLocation(LayoutDatetime, string(sv), loc)
@@ -33,6 +45,21 @@ func Str2Time(timestr string) (t time.Time, err error) {
 			t, err = time.ParseInLocation(LayoutShortdateTime, string(sv), loc)
 			if err != nil {
 				t, err = time.ParseInLocation(LayoutShortdateShortTime, string(sv), loc)
+				if err != nil {
+					t, err = time.ParseInLocation(LayoutDateMin, string(sv), loc)
+					if err != nil {
+						t, err = time.ParseInLocation(SlashDatetime, string(sv), loc)
+						if err != nil {
+							t, err = time.ParseInLocation(SlashShortdateTime, string(sv), loc)
+							if err != nil {
+								t, err = time.ParseInLocation(SlashShortdateShortTime, string(sv), loc)
+								if err != nil {
+									t, err = time.ParseInLocation(SlashDateMin, string(sv), loc)
+								}
+							}
+						}
+					}
+				}
 			}
 		}
 	}
