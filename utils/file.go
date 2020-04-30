035e736399dc643d10a5a7a0ed6f52e190e59abc
liting68 <liting68@163.com>
2020-04-30 15:47:20 +0800

    add utils

---
+++ utils/file.go
@@ -0,0 +1,12 @@
+package utils
+
+import "os"
+
+//FileExists 判断文件是否存在
+func FileExists(filename string) bool {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
+}
---
+++ utils/isutf8.go
@@ -0,0 +1,61 @@
+package utils
+
+import (
+	"github.com/axgle/mahonia"
+)
+
+func preNUm(data byte) int {
+	var mask byte = 0x80
+	var num int = 0
+	//8bit中首个0bit前有多少个1bits
+	for i := 0; i < 8; i++ {
+		if (data & mask) == mask {
+			num++
+			mask = mask >> 1
+		} else {
+			break
+		}
+	}
+	return num
+}
+
+//IsUTF8 是否是UTF8字符
+func IsUTF8(data []byte) bool {
+	i := 0
+	for i < len(data) {
+		if (data[i] & 0x80) == 0x00 {
+			// 0XXX_XXXX
+			i++
+			continue
+		} else if num := preNUm(data[i]); num > 2 {
+			// 110X_XXXX 10XX_XXXX
+			// 1110_XXXX 10XX_XXXX 10XX_XXXX
+			// 1111_0XXX 10XX_XXXX 10XX_XXXX 10XX_XXXX
+			// 1111_10XX 10XX_XXXX 10XX_XXXX 10XX_XXXX 10XX_XXXX
+			// 1111_110X 10XX_XXXX 10XX_XXXX 10XX_XXXX 10XX_XXXX 10XX_XXXX
+			// preNUm() 返回首个字节的8个bits中首个0bit前面1bit的个数，该数量也是该字符所使用的字节数
+			i++
+			for j := 0; j < num-1; j++ {
+				//判断后面的 num - 1 个字节是不是都是10开头
+				if (data[i] & 0xc0) != 0x80 {
+					return false
+				}
+				i++
+			}
+		} else {
+			//其他情况说明不是utf-8
+			return false
+		}
+	}
+	return true
+}
+
+//Array2UTF8 字符数组转UTF8
+func Array2UTF8(arr []string) []string {
+	for k, v := range arr {
+		if !IsUTF8([]byte(v)) {
+			arr[k] = mahonia.NewDecoder("gb18030").ConvertString(v)
+		}
+	}
+	return arr
+}
